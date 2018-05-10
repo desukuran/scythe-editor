bfaa37eb14d0e10ae71e20c34176e19e2548d35f
Buff Drinklots <dethklan@gmail.com>
2018-05-10 11:40:36 -0400

    When you load a file, a file tree 50% correctly populates You can now peek and poke parent nodes, but not child ones correctly.
    I am sure there is a better and easier way to do this.

--- scythe/Form1.Designer.cs
+++ scythe/Form1.Designer.cs
@@ -28,7 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node2");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node1");
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.playerNameTextBox = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -41,15 +50,70 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player Name";
+            // 
+            // playerNameTextBox
+            // 
+            this.playerNameTextBox.Location = new System.Drawing.Point(85, 71);
+            this.playerNameTextBox.Name = "playerNameTextBox";
+            this.playerNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.playerNameTextBox.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(15, 117);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node2";
+            treeNode1.Text = "Node2";
+            treeNode2.Name = "Node0";
+            treeNode2.Text = "Node0";
+            treeNode3.Name = "Node1";
+            treeNode3.Text = "Node1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode3});
+            this.treeView1.Size = new System.Drawing.Size(244, 482);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(265, 133);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(284, 20);
+            this.valueTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(265, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Value:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 409);
+            this.ClientSize = new System.Drawing.Size(749, 635);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.playerNameTextBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Scythe Editor";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -58,6 +122,11 @@
         private System.Windows.Forms.Button button1;
 
         public string loadedSaveData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox playerNameTextBox;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- scythe/Form1.cs
+++ scythe/Form1.cs
@@ -27,33 +27,77 @@ namespace scythe
 
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                XmlDocument doc = new XmlDocument();
+                isPopulatingList = true;
+                treeView1.Nodes.Clear();
+                doc = new XmlDocument();
                 doc.Load(openFileDialog1.FileName);
                 loadedSaveData = doc.InnerXml;
 
-                using (XmlReader reader = XmlReader.Create(openFileDialog1.FileName))
-                {
-                    while (reader.Read())
-                    {
-                        // Only detect start elements.
-                        if (reader.IsStartElement())
-                        {
-                            // Get element name and switch on it.
-                            switch (reader.Name)
-                            {
-                                case "player":
-                                    reader.ReadToDescendant("name");
-                                    Text = reader.ReadElementContentAsString();
-                                    break;
+                string str = null;
 
-                                case "name":
-                                    
-                                    break;
-                            }
-                        }
+                xmlnode = doc.GetElementsByTagName("player");
+                for (int i = 0; i <= xmlnode[0].ChildNodes.Count - 1; i++)
+                {
+                    //Get player name
+                    if (xmlnode[0].ChildNodes.Item(i).Name == "name")
+                    {
+                        playerNameTextBox.Text = xmlnode[0].ChildNodes.Item(i).InnerText.Trim();
+                    }
+
+                    TreeNode workingNode = treeView1.Nodes.Add(xmlnode[0].ChildNodes.Item(i).Name);
+                    treeView1.SelectedNode = workingNode;
+
+                    if (xmlnode[0].ChildNodes[i].ChildNodes.Count > 1)
+                    {
+                        for (int o = 0; o < xmlnode[0].ChildNodes[i].ChildNodes.Count; o++)
+                            workingNode = treeView1.SelectedNode.Nodes.Add(xmlnode[0].ChildNodes[i].ChildNodes.Item(o).Name);
+
+                    }
+                }
+
+            }
+
+            isPopulatingList = false;
+            treeView1.SelectedNode = treeView1.Nodes[0];
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (doc == null)
+                return;
+
+            if (isPopulatingList)
+                return;
+
+            if (treeView1.SelectedNode.Parent == null)
+            {
+                for (int i = 0; i <= xmlnode[0].ChildNodes.Count - 1; i++)
+                {
+                    if (xmlnode[0].ChildNodes.Item(i).Name == treeView1.SelectedNode.Text)
+                    {
+                        valueTextBox.Text = xmlnode[0].ChildNodes.Item(i).InnerXml.Trim();
+                        break;
                     }
                 }
             }
+            else
+            {
+                for (int i = 0; i <= xmlnode[0].ChildNodes.Count - 1; i++)
+                {
+                    if (xmlnode[0].ChildNodes.Item(i).Name == treeView1.SelectedNode.Text)
+                    {
+                        valueTextBox.Text = xmlnode[0].ChildNodes.Item(i).InnerXml.Trim();
+                        break;
+                    }
+                }
+            }
+
+
         }
+
+        public XmlNodeList xmlnode;
+        public XmlDocument doc;
+        private bool isPopulatingList;
     }
 }
