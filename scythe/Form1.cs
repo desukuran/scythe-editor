a24bc71abba9be18fb8f3489125f98b313de70a4
Buff Drinklots <dethklan@gmail.com>
2018-05-10 00:42:46 -0400

    Program now returns player's name in the program title.
    Absolute start of something magical, maybe.

--- scythe/Form1.cs
+++ scythe/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace scythe
 {
@@ -15,6 +16,44 @@ namespace scythe
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Save Files|*.*";
+            openFileDialog1.Title = "Select a Save File";
+
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(openFileDialog1.FileName);
+                loadedSaveData = doc.InnerXml;
+
+                using (XmlReader reader = XmlReader.Create(openFileDialog1.FileName))
+                {
+                    while (reader.Read())
+                    {
+                        // Only detect start elements.
+                        if (reader.IsStartElement())
+                        {
+                            // Get element name and switch on it.
+                            switch (reader.Name)
+                            {
+                                case "player":
+                                    reader.ReadToDescendant("name");
+                                    Text = reader.ReadElementContentAsString();
+                                    break;
+
+                                case "name":
+                                    
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
